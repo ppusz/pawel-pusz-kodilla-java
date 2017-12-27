5c0574a86e00329e336a3fdc947dd1062823e7ab
ppusz <pawel@pusz.name>
2017-12-27 23:34:38 +0100

    13.3 temp

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskFinancialDetailsDao.java
@@ -0,0 +1,9 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.TaskFinancialDetails;
+import org.springframework.data.repository.CrudRepository;
+
+public class TaskFinancialDetailsDao extends CrudRepository<TaskFinancialDetails, Integer> {
+
+
+}
